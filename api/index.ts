fe37fb5ca32ab00ad575fc0f4791fa28bdc540e3
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-10-01 23:39:12 +0600

    api: initial commit

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import expressWs from 'express-ws';
+import cors from 'cors';
+
+const app = express();
+expressWs(app);
+
+const port = 8000;
+
+app.use(cors());
+
+const router = express.Router();
+
+router.ws('/chat', (ws, req) => {
+  console.log('Client connected...');
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port...`)
+});
