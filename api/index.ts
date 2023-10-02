492e306e8b52e8890a5d29686a3b1f26f70c7c9d
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-10-02 12:10:12 +0600

    api: added websocket message route functionality

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import expressWs from 'express-ws';
 import cors from 'cors';
+import * as crypto from 'crypto';
+import { ActiveConnections, IncomingMessage } from './types';
 
 const app = express();
 expressWs(app);
@@ -11,8 +13,43 @@ app.use(cors());
 
 const router = express.Router();
 
+const activeConnections: ActiveConnections = {};
+
 router.ws('/chat', (ws, req) => {
-  console.log('Client connected...');
+  const id = crypto.randomUUID();
+  console.log('Client connected! id=', id);
+  activeConnections[id] = ws;
+
+  ws.on('close', () => {
+    console.log('Client disconnected! id=', id);
+    delete activeConnections[id];
+  });
+
+  let username = 'Anonymous';
+
+  ws.on('message', (msg) => {
+    const { type, payload } = JSON.parse(msg.toString()) as IncomingMessage;
+
+    switch (type) {
+      case 'SET_USERNAME':
+        username = payload;
+        break;
+      case 'SEND_MESSAGE':
+        Object.keys(activeConnections).forEach(connId => {
+          const conn = activeConnections[connId];
+          conn.send(JSON.stringify({
+            type: 'NEW_MESSAGE',
+            payload: {
+              username,
+              text: payload
+            },
+          }));
+        });
+        break;
+      default:
+        console.log('Unknown message type:', type);
+    }
+  });
 });
 
 app.use(router);
